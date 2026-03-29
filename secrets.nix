a5016ea36893075f1ba6fc4f73b724caf4586974
Abdullah As-Sadeed <bitscoper@protonmail.com>
2026-03-29 20:44:08 +0600

    System Version: March 27, 2026

--- secrets.nix
+++ secrets.nix
@@ -1,6 +1,33 @@
 # By Abdullah As-Sadeed
 
 {
-  password_1_of_bitscoper = "*****************";
-  password_2_of_bitscoper = "*****************";
+  config,
+  lib,
+  options,
+  ...
+}:
+{
+  options.secrets = {
+    password_1 = lib.mkOption {
+      type = lib.types.str;
+      internal = true;
+      readOnly = false;
+      description = "Password 1";
+      default = "*****************";
+      example = "*****************";
+    };
+    password_2 = lib.mkOption {
+      type = lib.types.str;
+      internal = true;
+      readOnly = false;
+      description = "Password 2";
+      default = "*****************";
+      example = "*****************";
+    };
+  };
+
+  config.secrets = {
+    password_1 = "*****************";
+    password_2 = "*****************";
+  };
 }
